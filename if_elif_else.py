ed5139f4b34c6398283b48bd568c143ed168122b
Shital_Ajit_Shelar <107055765+sas2055@users.noreply.github.com>
2023-01-07 12:43:17 +0530

    if_elif_else.py

---
+++ if_elif_else.py
@@ -0,0 +1,94 @@
+"""
+# Python if elif practice codes:
+------------------------------------------------
+
+current = float(input('Enter the current voltage: '))
+if current > 70:
+    print('Its High Voltage')
+elif current > 50:
+    print('Its Medium Voltage')
+else:
+    print('Its Low Voltage')
+==============================================
+
+speed = float(input('Enter the current speed:'))
+if speed > 80:
+    print('Its High speed')
+elif speed > 50:
+    print('Its Medium speed')
+else:
+    print('Its Low speed')
+===============================================
+
+space = float(input('Enter the space: '))
+if space > 1000:
+    print('more space area')
+elif space > 500:
+    print('medium space area')
+else:
+    print('low space area')
+==============================================
+
+sal = int(input('enter the salary: '))
+if int(sal) > 40000:
+    print('its travelling time to out of maharashtra')
+elif int(sal) > 20000:
+    print('its travelling time to near places of maharashtra')  
+else:
+    print('not possible travelling')
+=====================================================
+
+sport = str(input('which game u like: '))
+if sport.lower() == 'swimming':
+    print('i like game swimming')
+elif sport == 'karate':
+    print('this is karate')
+else:
+    print('I like to Dance') 
+======================================================
+
+wt = str(input('enter watch company: '))
+if wt == 'Fastrak':
+    print(wt, 'watch having quality.')
+elif wt == 'Rado':
+    print(wt, 'watch having quality.')
+else:
+    print(wt, 'low qulity watch')
+======================================================
+
+money = int(input('enter the money: '))
+if money > 500:
+    print('I want to eat Pizza.')
+elif money > 200:
+    print('I want to eat Dosa.')
+else:
+    print('I eat Vada-Pav')
+====================================================
+
+covid_test = input('is covid-19 test done(yes/no): ')
+if covid_test == 'yes':
+    print('covid-19 test done. ')
+elif covid_test == 'no':
+    print('do covid-19 test 1st')
+else:
+    print('covid-19 take precaution use Mask.')
+=====================================================
+
+brw = str(input('enter the brower name: '))
+if brw == 'google chrome':
+    print('this', brw, 'browser uses mostly')
+elif brw == 'mozilla':
+    print('this', brw, 'browser uses sometime')
+else:
+    print('This is', brw, 'not using mostly')
+======================================================
+
+v = input('vegetable available in home(yes/no): ')
+if v == 'yes':
+    print('cook delicious')
+elif v == 'no':
+    print('there is no vegetables')
+else:
+    print('bring vegetable with you.')
+==================================================
+"""
