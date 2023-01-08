03956850424e2ec01c453c6874399241b93c0ee7
Shital_Ajit_Shelar <107055765+sas2055@users.noreply.github.com>
2023-01-08 13:23:03 +0530

    if_else.py

---
+++ if_else.py
@@ -0,0 +1,106 @@
+"""
+# Python If else practice codes:
+------------------------------------------
+
+a = int(input('Enter your Final year percentage: '))
+if a >= 35:
+    print('Congragulations! You are pass.')
+else:
+    print('Sorry, You are fail')
+---------------------------------------------------------
+
+a = int(input('Enter current temprature is: '))
+if a <= 20:
+    print('AC not required.')
+els
+
+
+
+
+
+e:
+    print('Please turn on the AC.')
+------------------------------------------------------------
+
+print('Calculate Car average')
+a = int(input('Enter Total Fuel consumed in liters: '))
+b = int(input('Enter Total running KM: '))
+if a == 0 and b == 0:
+    print('Please enter valid KM and fuel consumed')
+else:
+    c = b/a;
+    print('Tentetive Average fuel econamy is: ', c)
+-----------------------------------------------------------
+
+print('Best Travaling ways')
+a = int(input('Enter current Temprature Outside: '))
+d = input('Enter Weather condition (Rainy/Cloudy/Clear): ')
+if a < 35 and d != 'Clear':
+    print('Do not use Two wheeler, Please prefer Cab or public Transport')
+else:
+    print('Good for two wheeler')
+------------------------------------------------------------
+
+a = int(input('Enter Availalbe cash: '))
+if a >= 500000 and a >=100000:
+    print('You can purchase a new car')
+else:
+    print('Go with old car purchase')
+------------------------------------------------------------
+
+num = int(input('Enter any number: '))
+if num >= 0:
+    print('This is Positive number')
+else:
+    print('This is Negetive number')
+------------------------------------------------------------
+
+light = str(input('is light available(yes/ no): '))
+if light == 'yes':
+    print('on the music system')
+else:
+    print('no music day')
+-------------------------------------------------------------
+
+teacher = str(input('is teacher present: '))
+if teacher == 'yes':
+    print('class is there')
+else:
+    print('no class')
+------------------------------------------------------------
+
+rain = str(input('enter rain is there: '))
+if rain == 'yes':
+    print('sit at home')
+else:
+    print('go to school)
+------------------------------------------------------------
+          
+channel = str(input('enter which channel u like: '))
+if channel == 'netflix':
+    print('watching netflix')
+else:
+    print('watching movies')
+-----------------------------------------------------------
+
+veg = str(input('enter answer as(yes/ no): '))
+if veg == 'yes':
+    print('cook foods')
+else:
+    print('bring vegetables')
+--------------------------------------------------------------
+
+amt = int(input('Enter ammount: '))
+if amt > 5000:
+    print('discount is 50%')
+else:
+    print('discount is 10%')
+----------------------------------------------------------------
+
+report = input('the test report is(+ve / -ve): ')
+if report == '+ve':
+    print('admit + meditition')
+else:
+    print('it 14 days isolation')
+-------------------------------------------------------------    
+"""    
